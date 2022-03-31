3df64f266eac1de2e55ad681c9e63c5dc8579d79
Matthias <me@motz.xyz>
2022-03-31 09:10:00 +0200

    wip breeding dispenser

--- src/main/java/xyz/motz/classe/listeners/DispenserListener.java
+++ src/main/java/xyz/motz/classe/listeners/DispenserListener.java
@@ -3,6 +3,8 @@ package xyz.motz.classe.listeners;
 import de.tr7zw.nbtapi.NBTTileEntity;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.Particle;
 import org.bukkit.block.Dispenser;
 import org.bukkit.block.data.Directional;
 import org.bukkit.entity.Animals;
@@ -11,45 +13,48 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.RayTraceResult;
+import xyz.motz.classe.Classe;
 import xyz.motz.classe.util.mechanics.BreedingDispenser;
 
 public class DispenserListener implements Listener {
     @EventHandler
     public void onDispense(BlockDispenseEvent e) {
-        if (BreedingDispenser.isBreedingDispenser(e.getBlock())) {
-            Bukkit.getServer().sendMessage(Component.text("breeding dispenser activated"));
-            Dispenser dispenser = (Dispenser) e.getBlock().getState();
-            Directional dispenserDirection = (Directional) dispenser.getBlockData();
+        if (e.getBlock().getState() instanceof Dispenser dispenser) {
+            if (Classe.getInstance().getConfig().getBoolean("dispenser.can-breed-animals")) {
+                Directional dispenserDirection = (Directional) dispenser.getBlockData();
 
-            Bukkit.getServer().sendMessage(Component.text(dispenser.getLocation().toString()));
-            Bukkit.getServer().sendMessage(Component.text(dispenserDirection.getFacing().getDirection().toString()));
+                Bukkit.getServer().sendMessage(Component.text(dispenser.getLocation().toString()));
+                Bukkit.getServer().sendMessage(Component.text(dispenserDirection.getFacing().getDirection().toString()));
 
-            RayTraceResult result = e.getBlock().getWorld().rayTraceEntities(dispenser.getLocation(), dispenserDirection.getFacing().getDirection(), 2, 0.1);
-            if (result == null) {
-                e.setCancelled(true);
-                Bukkit.getServer().sendMessage(Component.text("stage 1"));
-                return;
-            }
+                RayTraceResult result = e.getBlock().getWorld().rayTraceEntities(dispenser.getLocation(), dispenserDirection.getFacing().getDirection(), 2, 0.1,
+                        (Entity entity) -> {
+                            if (entity instanceof Animals animal) {
+                                return animal.canBreed() && animal.isAdult() && animal.getLoveModeTicks() == 0;
+                            } else return false;
+                        });
+                if (result == null) {
+                    Bukkit.getServer().sendMessage(Component.text("nothing to breed!"));
+                    return;
+                }
 
-            Entity entity = result.getHitEntity();
-            if (entity == null) {
-                Bukkit.getServer().sendMessage(Component.text("stage 2"));
-                return;
-            }
-            Bukkit.getServer().sendMessage(Component.text(entity.getLocation().toString()));
+                Entity entity = result.getHitEntity();
+                if (entity == null) return;
 
-            if (!(entity instanceof Animals animal)) {
-                Bukkit.getServer().sendMessage(Component.text("stage 3"));
-                return;
+                Bukkit.getServer().sendMessage(Component.text(entity.toString()));
+
+                if (entity instanceof Animals animal) {
+                    Bukkit.getServer().sendMessage(Component.text(animal.canBreed()));
+                    if (animal.canBreed() && animal.isBreedItem(e.getItem())) {
+                        // breeding success
+                        animal.setLoveModeTicks(600);
+
+                        animal.getWorld().spawnParticle(Particle.HEART, animal.getLocation().add(0, 1, 0), 1);
+                        Bukkit.getServer().sendMessage(Component.text("breeded!"));
+                    }
+                }
             }
-            Bukkit.getServer().sendMessage(Component.text(animal.canBreed()));
-            if (animal.canBreed() && animal.isBreedItem(e.getItem())) {
-                // breeding success
-                animal.setBreed(true);
-                Bukkit.getServer().sendMessage(Component.text("breeded"));
-            }
-            Bukkit.getServer().sendMessage(Component.text("done"));
         }
     }
 
